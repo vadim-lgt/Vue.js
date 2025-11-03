5ff19b4939360190761fe673b0f41ac368546159
vadim-lgt <Vsudeev@bk.ru>
2025-11-04 06:52:14 +0800

    Start project

---
+++ src/stores/authStore.js
@@ -0,0 +1,67 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null, // Данные пользователя
+    token: localStorage.getItem('token') || null, // Токен из localStorage
+    isAuthenticated: false, // Статус аутентификации
+    errorMessage: "", // Ошибка, если есть
+  }),
+
+  actions: {
+    async login(credentials) {
+      this.errorMessage = "";
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/login', credentials);
+        this.token = response.data.token;
+        this.user = response.data.user;
+        this.isAuthenticated = true;
+        localStorage.setItem('token', response.data.token);
+      } catch (error) {
+        if (error.response) {
+          // Сервер ответил с ошибкой (код вне диапазона 2xx)
+          this.errorMessage = error.response.data.message;
+          console.log(error);
+        } else if (error.request) {
+          // Запрос был отправлен, но ответа не получено
+          this.errorMessage = error.message;
+          console.log(error);
+        } else {
+          // Что-то пошло не так при настройке запроса
+          console.log(error);
+        }
+      }
+    },
+
+    async getUser() {
+      this.errorMessage = "";
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/user', {
+          headers: {
+            Authorization: 'Bearer ' + this.token,
+          },
+        });
+        this.user = response.data;
+      } catch (error) {
+        if (error.response) {
+          this.errorMessage = error.response.data.message;
+          console.log(error);
+        } else if (error.request) {
+          this.errorMessage = error.message;
+          console.log(error);
+        } else {
+          console.log(error);
+        }
+      }
+    },
+
+    logout() {
+      this.token = null;
+      this.user = null;
+      this.isAuthenticated = false;
+      // Удаляем токен из localStorage
+      localStorage.removeItem('token');
+    },
+  },
+});
